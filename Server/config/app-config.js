3c1c70838820db307f5bc796b4b3923fa8f6afe2
Rohan Kangale <Rohan.Kangale@ezest.local>
2018-12-17 17:27:05 +0530

    DB url updated

--- Server/config/app-config.js
+++ Server/config/app-config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'db_url': 'mongodb://127.0.0.1:27017/local'
+    // 'db_url': 'mongodb://127.0.0.1:27017/local'
+    'db_url': "mongodb://chatbot123:chatbot123@ds257838.mlab.com:57838/heroku_d94vwjgw"
 };
